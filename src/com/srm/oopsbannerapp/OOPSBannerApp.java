c5532515c590a009de189a2729db1c80f1a26c42
BL-lakshmi <lakshmi.muthyalppa@bridgelabz.com>
2026-01-09 16:53:34 +0530

    UC3: print oops using String.join

--- src/com/srm/oopsbannerapp/OOPSBannerApp.java
+++ src/com/srm/oopsbannerapp/OOPSBannerApp.java
@@ -1,20 +1,20 @@
 /**
- * OOPSBannerApp UC2 - OOPS Banner Display Application
+ * OOPSBannerApp UC3 - OOPS Banner Display Application
  * This class demonstrates a simple java application that displays the object-  * oriented programming system oops acronym to the console.
  *@author Developer
- * @version 2.0
+ * @version 3.0
  */
 package com.srm.oopsbannerapp;
 
 public class OOPSBannerApp {
     public static void main(String args[]){
-            System.out.println("   ***   " + " " +"   ***   "+ " " +"******   "+ " "+"   ***** ");
-            System.out.println(" **   ** " + " " +" **   ** "+ " " +"**    ** "+ " "+" **      ");
-            System.out.println("**     **" + " " +"**     **"+ " " +"**     **"+ " "+"**       ");
-            System.out.println("**     **" + " " +"**     **"+ " " +"*******  "+ " "+"  *****  ");
-            System.out.println("**     **" + " " +"**     **"+ " " +"**       "+ " "+"      ** ");
-            System.out.println(" **   ** " + " " +" **   ** "+ " " +"**       "+ " "+"**    ** ");
-            System.out.println("   ***   " + " " +"   ***   "+ " " +"**       "+ " "+"  *****  ");
+        System.out.println(String.join(" ","   ***   " ,"   ***   ","******   ","   ***** "));
+        System.out.println(String.join(" "," **   ** " ," **   ** ","**    ** "," **      "));
+        System.out.println(String.join(" ","**     **" ,"**     **","**     **","**       "));
+        System.out.println(String.join(" ","**     **" ,"**     **","*******  ","  *****  "));
+        System.out.println(String.join(" ","**     **" ,"**     **","**       ","      ** "));
+        System.out.println(String.join(" "," **   ** " ," **   ** ","**       ","**    ** "));
+        System.out.println(String.join(" ","   ***   " ,"   ***   ","**       ","  *****  "));
 
     }
 
