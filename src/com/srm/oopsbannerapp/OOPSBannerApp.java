e87b34315a83d9f8a5ddb15e199713242e67aa18
BL-lakshmi <lakshmi.muthyalppa@bridgelabz.com>
2026-01-09 17:13:03 +0530

    UC5: print oops using Inline StringArray

--- src/com/srm/oopsbannerapp/OOPSBannerApp.java
+++ src/com/srm/oopsbannerapp/OOPSBannerApp.java
@@ -1,25 +1,24 @@
 /**
- * OOPSBannerApp UC4 - OOPS Banner Display Application
+ * OOPSBannerApp UC5 - OOPS Banner Display Application
  * This class demonstrates a simple java application that displays the object-  * oriented programming system oops acronym to the console.
  *@author Developer
- * @version 4.0
+ * @version 5.0
  */
 package com.srm.oopsbannerapp;
 
 public class OOPSBannerApp {
     public static void main(String args[]){
-        String lines[]=new String[7];
-        lines[0] = String.join(" ","   ***   " ,"   ***   ","******   ","   ***** ");
-        lines[1] = String.join(" "," **   ** " ," **   ** ","**    ** "," **      ");
-        lines[2] = String.join(" ","**     **" ,"**     **","**     **","**       ");
-        lines[3] = String.join(" ","**     **" ,"**     **","*******  ","  *****  ");
-        lines[4] = String.join(" ","**     **" ,"**     **","**       ","      ** ");
-        lines[5] = String.join(" "," **   ** " ," **   ** ","**       ","**    ** ");
-        lines[6] = String.join(" ","   ***   " ,"   ***   ","**       ","  *****  ");
+        String lines[]= {
+                String.join(" ", "   ***   ", "   ***   ", "******   ", "   ***** "),
+                String.join(" ", " **   ** ", " **   ** ", "**    ** ", " **      "),
+                String.join(" ", "**     **", "**     **", "**     **", "**       "),
+                String.join(" ", "**     **", "**     **", "*******  ", "  *****  "),
+                String.join(" ", "**     **", "**     **", "**       ", "      ** "),
+                String.join(" ", " **   ** ", " **   ** ", "**       ", "**    ** "),
+                String.join(" ", "   ***   ", "   ***   ", "**       ", "  *****  ")
+        };
         for(String line:lines){
             System.out.println(line);
         }
-
     }
-
 }
