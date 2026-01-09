25be9d08b8abc6d23a8c35fdaa3708cac8f3e6dc
BL-lakshmi <lakshmi.muthyalppa@bridgelabz.com>
2026-01-09 17:18:45 +0530

    UC6: print oops using logic in methods

--- src/com/srm/oopsbannerapp/OOPSBannerApp.java
+++ src/com/srm/oopsbannerapp/OOPSBannerApp.java
@@ -1,24 +1,55 @@
 /**
- * OOPSBannerApp UC5 - OOPS Banner Display Application
+ * OOPSBannerApp UC6 - OOPS Banner Display Application
  * This class demonstrates a simple java application that displays the object-  * oriented programming system oops acronym to the console.
  *@author Developer
- * @version 5.0
+ * @version 6.0
  */
 package com.srm.oopsbannerapp;
 
 public class OOPSBannerApp {
-    public static void main(String args[]){
-        String lines[]= {
-                String.join(" ", "   ***   ", "   ***   ", "******   ", "   ***** "),
-                String.join(" ", " **   ** ", " **   ** ", "**    ** ", " **      "),
-                String.join(" ", "**     **", "**     **", "**     **", "**       "),
-                String.join(" ", "**     **", "**     **", "*******  ", "  *****  "),
-                String.join(" ", "**     **", "**     **", "**       ", "      ** "),
-                String.join(" ", " **   ** ", " **   ** ", "**       ", "**    ** "),
-                String.join(" ", "   ***   ", "   ***   ", "**       ", "  *****  ")
+    public static String[] getOPattern(){
+        return new String[]{
+                "   ***   ",
+                " **   ** ",
+                "**     **",
+                "**     **",
+                "**     **",
+                " **   ** ",
+                "   ***   "
         };
-        for(String line:lines){
-            System.out.println(line);
+    }
+
+    public static String[] getPPattern(){
+        return new String[]{
+                "******   ",
+                "**    ** ",
+                "**     **",
+                "*******  ",
+                "**       ",
+                "**       ",
+                "**       "
+        };
+    }
+
+    public static String[] getSPattern(){
+        return new String[]{
+                "   ***** ",
+                " **      ",
+                "**       ",
+                "  *****  ",
+                "      ** ",
+                "**    ** ",
+                "  *****  "
+
+        };
+    }
+
+    public static void main(String args[]){
+        String[] oPattern= getOPattern();
+        String[] pPattern= getPPattern();
+        String[] sPattern= getSPattern();
+        for(int i=0;i< oPattern.length;i++){
+            System.out.println(oPattern[i] + " " + oPattern[i] + " " + pPattern[i] + " " + sPattern[i]);
         }
     }
 }
