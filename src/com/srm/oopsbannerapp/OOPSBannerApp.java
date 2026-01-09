f697e7f197a4a05fdb5466c087e47726f3c6471b
BL-lakshmi <lakshmi.muthyalppa@bridgelabz.com>
2026-01-09 16:38:36 +0530

    UC1: print OOPS in console

--- src/com/srm/oopsbannerapp/OOPSBannerApp.java
+++ src/com/srm/oopsbannerapp/OOPSBannerApp.java
@@ -1,10 +1,16 @@
+/**
+ * OOPSBannerApp UC1 - OOPS Banner Display Application
+ * This class demonstrates a simple java application that displays the object-  * oriented programming system oops acronym to the console.
+ *@author Developer
+ * @version 1.0
+ */
 package com.srm.oopsbannerapp;
 
 import org.w3c.dom.ls.LSOutput;
 
 public class OOPSBannerApp {
     public static void main(String args[]){
-        System.out.println();
+        System.out.println("OOPS");
     }
 
 }
